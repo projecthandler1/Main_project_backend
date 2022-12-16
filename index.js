59194bc85b59bf6732e9bdc1ef81eb8c56f1f98b
dhruv patel <dhruv160916@gmail.com>
2022-12-16 10:39:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require("http"); 
+//create a server object: 
+http 
+  .createServer(function (req, res) { 
+    res.write("<h1>Hello World!</h1>");  
+    //write a response to the client 
+     
+    res.end();  
+    //end the response 
+  }) 
+  .listen(8000);  
