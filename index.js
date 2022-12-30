6aefbfd0b4953ac5f4b4efb5e191b21dce68cd1c
projecthandler1 <120641821+projecthandler1@users.noreply.github.com>
2022-12-30 13:23:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
-const http = require("http"); 
-//create a server object: 
-http 
-  .createServer(function (req, res) { 
-    res.write("<h1>Hello World!</h1>");  
-    //write a response to the client 
-     
-    res.end();  
-    //end the response 
-  }) 
-  .listen(8000);  
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+
+mongoose.connect("mongodb://127.0.0.1:27017/myLoginRegisterDB",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+},()=>{
+    console.log("DB connected")
+})
+
+const userSchema = new mongoose.Schema({
+      name: String,
+      email: String,
+      password: String
+})
+
+const User = new mongoose.model("User",userSchema)
+
+
+
+app.post("/SignUp",(req,res)=>{
+    // res.send("My API SignUp Page")
+    // console.log(req.body);
+    const {name,email,password} = req.body
+    User.findOne({email:email},(err,user)=>{
+        if(user){
+            res.send({message:"User already Registered."})
+        }else{
+            const user  = new User({
+                name,
+                email,
+                password
+            })
+            user.save(err=>{
+                if(err){
+                    res.send(err);
+                }else{
+                    res.send({message: "Succesfully Registered."});
+                }
+            })
+        }
+    })
+})
+
+app.listen(9002,()=>{
+    console.log("BE started at port 9002")
+})
